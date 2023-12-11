0420c50b4c1ca3938bc82dde1629f7eadaf8f805
Vinícius Batista <112179061+ViBatista0@users.noreply.github.com>
2023-12-11 17:56:08 -0300

    Criando projeto

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            //HtmlEncoder.Default.Encode previne BO, coisas má intencionada.
+            //O $ indica que vai ter coisa inserida, um parâmetro.
+            // return HtmlEncoder.Default.Encode($"Oi {name}, ID: {ID}");
+
+            ViewData["Message"] = "Oi " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+
+        }
+    }
+}
